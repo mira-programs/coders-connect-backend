087093f2befb579ad6d4437a62e78b1b3c936d2d
mira-programs <mirahussein06@gmail.com>
2024-11-07 16:00:32 +0300

    comments, likes, dislikes attributes for posts

--- models/Post.js
+++ models/Post.js
@@ -3,8 +3,15 @@ const postSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     content: { type: String, required: true },
     media: { type: String, required: false },
-    privacy: {type: String, required: true, default: "private"},
-    createdAt: { type: Date, default: Date.now }
+    privacy: { type: String, required: true, default: "private" },
+    createdAt: { type: Date, default: Date.now },
+    likes: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+    dislikes: [{ type: mongoose.Schema.ObjectId, ref: "User" }],
+    comments: [{
+        text: String,
+        created: { type: Date, default: Date.now },
+        postedBy: { type: mongoose.Schema.ObjectId, ref: "User" },
+    }],
 });
 const Post = mongoose.model('Post', postSchema);
 module.exports = Post;
