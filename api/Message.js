5c1320cae67dee9b04249cbc12beb45ca2c6ebef
mira-programs <mirahussein06@gmail.com>
2024-11-11 13:41:30 +0300

    Update Message.js
    to be tested

--- api/Message.js
+++ api/Message.js
@@ -1,12 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const verifyToken = require('../middleware/verifyToken');
+import Chat from '../models/Chat';
 
 router.post("/send/:id", verifyToken, async(req,res) => {
     try{
         const{message} = req.body;
-        const{id} = req.params;
-        const senderId = req.userId;
+        const{id: receiverId} = req.params;
+        const senderId = req.user._id;
+
+        const chat = await Chat.findOne({
+            participants: {$all: [senderId, receiverId]},
+        })
+
+        if(!chat){
+            chat = await Chat.create({
+                participants: [senderId, receiverId],
+            })
+        }
+
+        const newMessage = new Message({
+            senderId: senderId,
+            receiverId: receiverId,
+            message: message,
+        })
+
+        if(newMessage){
+            chat.messages.push(newMessage._id);
+        }
+
+        res.status.json(newMessage);
+
     }catch(error){
         console.log("error in send message: ", error.message);
         res.status(500).json({error:"internal server error"});
