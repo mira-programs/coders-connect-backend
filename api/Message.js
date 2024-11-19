9210f17a1f0cdec9d70a2117ed48a1fb855ac8c6
mira-programs <mirahussein06@gmail.com>
2024-11-19 15:00:40 +0300

    api to get users for sidebar on messages
    getting users that can be messaged to be shown on sidebar of messages page

--- api/Message.js
+++ api/Message.js
@@ -3,7 +3,22 @@ const router = express.Router();
 const verifyToken = require('../middleware/verifyToken');
 const Chat = require('../models/Chat');
 const Message = require('../models/Message');
+const User = require('../models/User');
 const mongoose = require('mongoose');
+
+router.get("/getUsersForSidebar",verifyToken, async(req,res) => {
+    try {
+        const loggedInUserId = req.user.userId;
+
+        const filteredUsers = await User.find({_id: {$ne: loggedInUserId}}).select("-password") //find all users except logged in user
+        
+        res.status(200).json(filteredUsers);
+
+    } catch (error) {
+        console.log("error in getusersforsidebar: ", error.message);
+        res.status(500).json({error: "internal server error"});
+    }
+})
 
 //get chat and messages between current user and another user
 router.get("/:id", verifyToken, async (req, res) => {
