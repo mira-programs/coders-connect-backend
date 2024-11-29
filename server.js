67a841a2524af9ace65d91dec702a027d6fe3c3b
mira-programs <mirahussein06@gmail.com>
2024-11-29 15:54:19 +0300

    changes for socketio

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-//mongodb
+// mongodb
 require('./config/db');
 require('dotenv').config();
 const UserRouter = require('./api/User');
@@ -7,25 +7,27 @@ const FriendshipRouter = require('./api/Friendship');
 const PostRouter = require('./api/Post');
 const MessageRouter = require('./api/Message');
 
-const express = require('express'); 
+const express = require('express');
 const cors = require('cors');
-const app = require('./socket/socket.js');
+const app = express();  // Directly using express without socket.io
 const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://127.0.0.1:5500',  // Frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use('/user', UserRouter);
 app.use('/account', AccountRouter);
 app.use('/friendship', FriendshipRouter);
 app.use('/post', PostRouter);
 app.use('/message', MessageRouter);
 
-
 const path = require('path');
-
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
